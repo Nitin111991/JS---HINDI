daab768a71fd508accc41a1e0c5f5fe8b66f1874
Nitin-college <rainitin725@gmail.com>
2023-12-31 01:16:07 +0530

    new files add

--- 01_basics/05_iterations/four.js
+++ 01_basics/05_iterations/four.js
@@ -14,3 +14,15 @@ const programming = ["js", "rb", "py", "java", "cpp"]
 for (const key in programming) {
    console.log(programming[key]);
 }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
